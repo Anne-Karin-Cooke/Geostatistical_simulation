be798a39a82c17d19fe2623c8c672463f2592d95
Anne-Karin-Cooke <56202898+Anne-Karin-Cooke@users.noreply.github.com>
2020-09-26 14:35:58 +0200

    Create sim.sh

---
+++ sim.sh
@@ -0,0 +1,10 @@
+cd /home/acooke/Sims/
+sshpass -p "password" ssh -XY cooke@localhost -p port_number "mkdir ~cluster/sim_repo/"
+#sshpass -p "password" ssh -XY cooke@localhost -p port_number "cp ~cluster/test.sh ~cluster/sim_repo/"
+sshpass -p "password" ssh -XY cooke@localhost -p port_number "cp ~cluster/gravity.conf ~cluster/sim_repo/"
+sshpass -p "password" ssh -XY cooke@localhost -p port_number "cp ~cluster/gravity.txt ~cluster/sim_repo/"
+sshpass -p "password" ssh -XY cooke@localhost -p port_number "cp ~cluster/test.in ~cluster/sim_repo/"
+for f in *.h5 ; do
+sshpass -p "password" scp -P port_number /home/acooke/Sims/$f cooke@localhost:~cluster/sim_repo;
+sshpass -p "password" ssh -XY cooke@localhost -p port_number "sh ~cluster/sim_repo/test.sh";
+done; 
